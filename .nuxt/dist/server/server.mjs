d0bd083de0cbedda2d3172d64e299d5cd6593326
Philippe VILARINHO <59869747+tifiloutube@users.noreply.github.com>
2023-10-04 18:19:31 +0200

    desktop version

--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/philippevilarinho/Documents/GitHub/my-portfolio/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- nuxt.config.js
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/style/global.css'],
-  postcss: {
-  },
-  target: 'static'
-})
